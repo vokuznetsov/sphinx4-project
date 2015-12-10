f2958f48a41ca9ad790725b4f34748d9153c2296
Kuznetsov Vladimir <vkuzn@mera.ru>
2015-12-10 16:43:32 +0300

    add speech aligner

---
+++ server-Sphinx4/aligner/src/main/java/Main.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+
+/**
+ * @author vkuzn on 10.12.2015.
+ */
+public class Main {
+    private static final String TEXT = "development";
+    //private static final String TEXT = "one zero zero zero one nine oh two one oh zero one eight zero three";
+    public static void main(String[] args){
+        try {
+            Aligner aligner = new Aligner(TEXT);
+            aligner.recognize();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
