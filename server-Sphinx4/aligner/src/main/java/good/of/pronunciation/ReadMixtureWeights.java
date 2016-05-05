20d397c05b7b4d303a3b7c738d28447cf1e6e19a
Kuznetsov Vladimir <vkuzn@mera.ru>
2016-05-05 12:39:00 +0300

    resize mixture weights from size 5126x384 to 5126x128

--- server-Sphinx4/aligner/src/main/java/good/of/pronunciation/ReadMixtureWeights.java
+++ server-Sphinx4/aligner/src/main/java/good/of/pronunciation/ReadMixtureWeights.java
@@ -8,6 +8,8 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 /**
  * @author vkuzn on 05.05.2016.
@@ -15,15 +17,15 @@ import java.util.Map;
 public class ReadMixtureWeights {
 
     private static final Logger log = LoggerFactory.getLogger(ReadMixtureWeights.class);
-    private final String mixw = "/models/acoustic/16khz/mixture_weights.txt";
+    private final String filename = "/models/acoustic/16khz/mixture_weights.txt";
 
     public ReadMixtureWeights() {
     }
 
     public Map<Integer, List<Double>> parseFile() throws IOException {
-        Map<Integer, List<Double>> result = new HashMap<>();
+        Map<Integer, List<Double>> mixw = new HashMap<>();
 
-        BufferedReader in = new BufferedReader(new InputStreamReader(openFile(mixw)));
+        BufferedReader in = new BufferedReader(new InputStreamReader(openFile(filename)));
         in.readLine();      // skip first string
 
         String str = null;
@@ -42,13 +44,12 @@ public class ReadMixtureWeights {
 
                 if (numberOfPhoneme != phoneme) {
                     numberOfPhoneme = phoneme;
-                    //result.put(count, new ArrayList<>(values));
                 }
 
                 param = Integer.parseInt(str.substring(closeBracket - 1, closeBracket));
                 if (param == 0) {
-                    if (!result.isEmpty()){
-                        result.get(count).addAll(new ArrayList<>(values));
+                    if (!mixw.isEmpty()){
+                        mixw.get(count).addAll(new ArrayList<>(values));
                         values.clear();
                     }
                     count++;
@@ -56,10 +57,10 @@ public class ReadMixtureWeights {
 
             } else if (str.equals("")) {
             } else {
-                if (result.get(count) == null) {
-                    result.put(count, new ArrayList<>(values));
+                if (mixw.get(count) == null) {
+                    mixw.put(count, new ArrayList<>(values));
                 } else {
-                    result.get(count).addAll(new ArrayList<>(values));
+                    mixw.get(count).addAll(new ArrayList<>(values));
                 }
                 values.clear();
 
@@ -78,8 +79,24 @@ public class ReadMixtureWeights {
                 }
             }
         }
-        result.get(count).addAll(new ArrayList<>(values));
+        mixw.get(count).addAll(new ArrayList<>(values));
         values.clear();
+
+        Map<Integer, List<Double>> result = new HashMap<>();
+
+        // разбиваем считанные значения на три части и суммируем их. - Не уверен в точности этого подхода.
+        // divide our mixture weights on three list and sum them. It means, that I resize our mix. weights from 5126x384 to 5126x128
+        for (int i=0; i <= count; i++){
+            List<Double> firstParam = mixw.get(i).subList(0, 128);
+            List<Double> secondParam = mixw.get(i).subList(128, 256);
+            List<Double> thirdParam = mixw.get(i).subList(256, 384);
+
+            List<Double> sum = IntStream.range(0, firstParam.size())
+                    .mapToObj(j -> firstParam.get(j) + secondParam.get(j) + thirdParam.get(j))
+                    .collect(Collectors.toList());
+            result.put(i, new ArrayList<>(sum));
+        }
+
         return result;
     }
 
